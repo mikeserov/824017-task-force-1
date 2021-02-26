ca2c47d43c605b322ae3d00ed2a14fce4d8814b2
Михаил <serov.m@yandex.ru>
2021-02-27 02:51:33 +0500

    module2-task1 50% completed

--- config/schema.sql
+++ config/schema.sql
@@ -1,16 +1,66 @@
-CREATE DATABASE yeti_cave
+CREATE DATABASE task_force_1
 	DEFAULT CHARACTER SET utf8
 	DEFAULT COLLATE UTF8_GENERAL_CI;
 
-USE yeti_cave;
+USE task_force_1;
 
-CREATE TABLE Tasks (
+CREATE TABLE tasks (
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	customer_id INT REFERENCES users(id),
+	executant_id INT REFERENCES users(id),
+	city_id INT REFERENCES cities(id),
+	posting_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- какой тип данных лучше выбрать, datetime или timezone..
+	status VARCHAR(50),
+	name VARCHAR(1000),
+	description VARCHAR(3000),
+	helpful_files VARCHAR(3000),
+	location VARCHAR(500),
+	payment VARCHAR(500),
+	deadline_date TIMESTAMP
+);
+
+CREATE TABLE users (
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	city_id INT REFERENCES cities(id),
+	signing_up_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	role VARCHAR(50),
+	name VARCHAR(300),
+	email VARCHAR(300),
+	password VARCHAR(3000),
+	avatar VARCHAR(1000),
+	birthday DATE,
+	self_description VARCHAR(3000),
+	accomplished_tasks_photos VARCHAR(5000),
+	phone VARCHAR(100),
+	skype VARCHAR(100),
+	telegram VARCHAR(100),
+	is_hidden_contacts BOOLEAN,
+	is_hidden_account BOOLEAN,
+	is_subscribed_messages BOOLEAN,
+	is_subscribed_actions BOOLEAN,
+	is_subscribed_reviews BOOLEAN,
+	favorite_count INT,
+	failure_count INT
+);
+
+CREATE TABLE notifications (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	user_id INT REFERENCES users(id),
+	task_id INT REFERENCES tasks(id),
+	type VARCHAR(100)
+);
+
+CREATE TABLE cities (
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(100)
+);
+
+CREATE TABLE responses (
 	id INTEGER AUTO_INCREMENT PRIMARY KEY,
 	author INT REFERENCES users(id),
-	author INT REFERENCES users(id),
-	author INT REFERENCES cities(id),
-	posting_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-)
+	winner INT REFERENCES users(id)
+);
+
 
 
 
@@ -19,13 +69,7 @@ CREATE TABLE Tasks (
 CREATE TABLE lots (
 	id INTEGER AUTO_INCREMENT PRIMARY KEY,
 	dt_start TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	name VARCHAR(128),
-	description TEXT(128),
-	img TEXT(128),
-	start_price DECIMAL,
-	dt_end TIMESTAMP,
-	rate_step DECIMAL,
-	category_id INTEGER REFERENCES categories(id),
+
 	author INT REFERENCES users(id),
 	winner INT REFERENCES users(id)
 );
