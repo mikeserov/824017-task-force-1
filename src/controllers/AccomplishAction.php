c7f2ee4358bee7695ed9f54b0cc3393cea7ae128
Михаил <serov.m@yandex.ru>
2021-03-02 14:58:56 +0500

    _

--- src/controllers/AccomplishAction.php
+++ src/controllers/AccomplishAction.php
@@ -6,8 +6,14 @@ namespace TaskForce\controllers;
 
 class AccomplishAction extends AbstractAction {
 	
-	public function getInternalActionName();
-	public function getDisplayingActionName();
+	public function getInternalActionName()
+	{
+		
+	}
+	public function getDisplayingActionName()
+	{
+		return 'to accomplish';
+	}
 	public function canUserAct(int executantId, int customerId, int currentUserId): bool
 	{
 		return customerId === currentUserId ? true : false;
