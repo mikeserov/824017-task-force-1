02865d4cf66fd658a4fb26f0f8d6416f8a530aac
Михаил <serov.m@yandex.ru>
2021-03-15 19:11:25 +0500

    _

--- others/converter-class-test.php
+++ others/converter-class-test.php
@@ -6,12 +6,10 @@ use TaskForce\Utils\CsvToSqlConverter;
 
 require_once 'vendor/autoload.php';
 
-$tables = ['cities','users','users_optional_settings','tasks','notifications_history','responses','chat_messages','reviews','specializations','user_specialization'];
+$tables = ['cities', 'users', 'users_optional_settings', 'tasks', 'notifications_history', 'responses', 'chat_messages', 'reviews', 'specializations', 'user_specialization'];
 
 foreach ($tables as $table) {
-    $converter = new CsvToSqlConverter($table . '.csv');
-
-    $converter->convert();
+    (new CsvToSqlConverter($table . '.csv'))->convert();
 }
 
 echo 'finished';
